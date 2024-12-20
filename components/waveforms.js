1e40ada96009e0091a8ae8425728ab2147d89a87
Ryan Hill <ryanjh88@gmail.com>
2024-12-20 11:25:58 -0600

    feat(wf links): add links to special waveforms

--- components/waveforms.js
+++ components/waveforms.js
@@ -12,48 +12,63 @@ function resizeCanvas() {
 let waveFunctions = [];
 
 class WaveFunction {
-  constructor() {
-    this.radius = 20 + Math.random() * 30; // Random radius between 20 and 50
+  constructor(isSpecial = false, label = '', url = '', color = '') {
+    this.radius = isSpecial ? 50 : 20 + Math.random() * 30;
     this.x = Math.random() * (canvas.width - 2 * this.radius) + this.radius;
     this.y = Math.random() * (canvas.height - 2 * this.radius) + this.radius;
-    this.speedX = (Math.random() - 0.5) * 0.5; // Slower speed
+    this.speedX = (Math.random() - 0.5) * 0.5;
     this.speedY = (Math.random() - 0.5) * 0.5;
-    this.alpha = 0.7; // Initial transparency
-    this.isCollapsing = false; // Flag to check if collapsing
-    this.collapseDuration = 500; // Duration of the collapse animation in milliseconds
-    this.collapseStartTime = null; // Timestamp when collapse starts
-    this.markedForRemoval = false; // New flag for removal
+    this.alpha = 0.7;
+    this.isCollapsing = false;
+    this.collapseDuration = 500;
+    this.collapseStartTime = null;
+    this.markedForRemoval = false;
+    this.isSpecial = isSpecial;
+    this.label = label;
+    this.url = url;
+    this.color = isSpecial ? color : 'rgba(100, 100, 255, 1)';
   }
 
   draw() {
     ctx.save();
     ctx.beginPath();
     ctx.globalAlpha = this.alpha;
-    ctx.fillStyle = 'rgba(100, 100, 255, 1)';
-    ctx.shadowColor = 'rgba(100, 100, 255, 1)';
-    ctx.shadowBlur = 15 * this.alpha; // Reduce shadow as it fades
+    ctx.fillStyle = this.color;
+    ctx.shadowColor = this.color;
+    ctx.shadowBlur = 15 * this.alpha;
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     ctx.fill();
     ctx.closePath();
+
+    if (this.isSpecial) {
+      ctx.fillStyle = 'white';
+      ctx.font = 'bold 32px Arial';
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      ctx.save();
+      ctx.translate(this.x, this.y);
+      ctx.scale(0.5, 0.5);
+      ctx.rotate(0.01);
+      ctx.fillText(this.label, 0, 0);
+      ctx.restore();
+    }
+
     ctx.restore();
   }
 
-  update() {
+  update(deltaTime) {
     if (this.isCollapsing) {
-      // Update alpha for fade-out effect
       const elapsed = Date.now() - this.collapseStartTime;
       this.alpha = Math.max(0, 0.7 * (1 - elapsed / this.collapseDuration));
-
       if (this.alpha <= 0) {
         this.markedForRemoval = true;
       }
-      return; // Skip movement updates during collapse
+      return;
     }
 
-    this.x += this.speedX;
-    this.y += this.speedY;
+    this.x += this.speedX * deltaTime * 60;
+    this.y += this.speedY * deltaTime * 60;
 
-    // Bounce off the walls
     if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
       this.speedX = -this.speedX;
     }
@@ -63,37 +78,65 @@ class WaveFunction {
   }
 
   collapse() {
-    this.isCollapsing = true;
-    this.collapseStartTime = Date.now();
+    if (!this.isSpecial) {
+      this.isCollapsing = true;
+      this.collapseStartTime = Date.now();
+    }
   }
 }
 
+// Add the special waveforms
+waveFunctions.push(
+  new WaveFunction(
+    true,
+    'LinkedIn',
+    'https://www.linkedin.com/in/ryan-james-hill/',
+    '#0077B5',
+  ),
+);
+waveFunctions.push(
+  new WaveFunction(true, 'GitHub', 'https://github.com/ryanhill1', '#171515'),
+);
+waveFunctions.push(
+  new WaveFunction(
+    true,
+    'QCSE',
+    'https://quantumcomputing.stackexchange.com/users/13991/ryanhill1?tab=profile',
+    '#F48024',
+  ),
+);
+
 // Add a new wave function every 2 seconds
 setInterval(() => {
-  if (waveFunctions.length < 10) {
-    // Limit the number of wave functions
+  if (waveFunctions.filter((wf) => !wf.isSpecial).length < 10) {
     waveFunctions.push(new WaveFunction());
   }
 }, 2000);
 
-// Updated Animation loop
-function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+const FIXED_TIME_STEP = 1000 / 60; // 60 FPS
+let lastTime = 0;
 
-  waveFunctions.forEach((wf) => {
-    wf.update();
-    wf.draw();
-  });
-
-  // Remove collapsed wave functions after iteration
-  waveFunctions = waveFunctions.filter((wf) => !wf.markedForRemoval);
-
+function animate(currentTime) {
   requestAnimationFrame(animate);
+
+  const deltaTime = (currentTime - lastTime) / 1000;
+
+  if (deltaTime >= FIXED_TIME_STEP / 1000) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    waveFunctions.forEach((wf) => {
+      wf.update(deltaTime);
+      wf.draw();
+    });
+
+    waveFunctions = waveFunctions.filter((wf) => !wf.markedForRemoval);
+
+    lastTime = currentTime;
+  }
 }
 
-animate();
+animate(0);
 
-// Updated click event listener
 canvas.addEventListener('click', function (event) {
   const rect = canvas.getBoundingClientRect();
   const clickX = event.clientX - rect.left;
@@ -101,9 +144,13 @@ canvas.addEventListener('click', function (event) {
 
   for (let wf of waveFunctions) {
     const distance = Math.hypot(wf.x - clickX, wf.y - clickY);
-    if (distance < wf.radius && !wf.isCollapsing) {
-      wf.collapse();
-      break; // Remove this line if you want to collapse multiple overlapping wave functions
+    if (distance < wf.radius) {
+      if (wf.isSpecial) {
+        window.open(wf.url, '_blank');
+      } else if (!wf.isCollapsing) {
+        wf.collapse();
+      }
+      break;
     }
   }
 });
